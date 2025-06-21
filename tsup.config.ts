b69433108526fdaf3acf09469301c53e72f65e6c
EDΞN. <EdenKneale@outlook.com>
2025-06-21 22:03:52 +0100

    Initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,42 @@
+import { fixImportsPlugin } from 'esbuild-fix-imports-plugin';
+import { copy as esbuildPluginCopier } from 'esbuild-plugin-copy';
+import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions';
+import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
+import { defineConfig, type Options } from 'tsup';
+
+const baseOptions: Options = {
+	clean: true,
+	splitting: true,
+	entry: ['src/**/*.ts', 'src/**/*.mts', 'src/**/*.tsx', 'src/**/*.cts'],
+	dts: true,
+	minify: true,
+	skipNodeModulesBundle: true,
+	sourcemap: true,
+	platform: 'node',
+	target: 'esnext',
+	tsconfig: 'tsconfig.json',
+	keepNames: true,
+	esbuildPlugins: [
+		esbuildPluginVersionInjector(),
+		esbuildPluginFilePathExtensions(),
+		fixImportsPlugin(),
+		esbuildPluginCopier(),
+	],
+	treeshake: true,
+};
+
+export default [
+	defineConfig({
+		...baseOptions,
+		outDir: 'lib/cjs',
+		format: 'cjs',
+		outExtension: () => ({ js: '.cjs' }),
+	}),
+	defineConfig({
+		...baseOptions,
+		outDir: 'lib/esm',
+		format: 'esm',
+		outExtension: () => ({ js: '.mjs', dts: '.d.mts' }),
+	}),
+];
+
